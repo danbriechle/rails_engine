dc41238c82c04ae9d1afd007953cb232ba4057ce
Daniel Briechle <danbriechle@hotmail.com>
2019-01-23 20:19:34 -0700

    adds relationship tests to items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  describe 'relationships' do
+  describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :unit_price }
     it { should validate_numericality_of(:unit_price).is_greater_than_or_equal_to(0) }
     it { should validate_presence_of :description }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :merchant}
+    it { should have_many :invoice_items}
+    it { should have_many(:invoices).through(:invoice_items)}
   end
   describe 'instance methods' do
     it '.best_day' do
