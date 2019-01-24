afcf583aa115ecf9355c6b992fd5932c86a86f79
Daniel Briechle <danbriechle@hotmail.com>
2019-01-23 20:32:42 -0700

    adds relationships to invoice items

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  describe 'validations' do
+  describe 'relationships' do
     it { should belong_to :item}
     it { should belong_to :invoice}
+  end
+
+
+  describe 'validations' do
+    it { should validate_presence_of :unit_price}
+    it { should validate_presence_of :quantity}
   end
 end
