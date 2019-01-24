2cc8bae5bae27441f7ec225a67a89a9a2be16a21
Daniel Briechle <danbriechle@hotmail.com>
2019-01-23 20:16:25 -0700

    adds validators to item and item spec

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,12 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  validates_presence_of :name, :description
+  validates :unit_price, presence: true, numericality: {
+    only_integer: false,
+    greater_than_or_equal_to: 0
+  }
+
   def best_day
     InvoiceItem.where(item_id: self.id)
     .order(:quantity)
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  describe 'relationships' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :unit_price }
+    it { should validate_numericality_of(:unit_price).is_greater_than_or_equal_to(0) }
+    it { should validate_presence_of :description }
+  end
   describe 'instance methods' do
     it '.best_day' do
       item = create(:item)
       customer = create(:customer)
       merchant = Merchant.find(item.merchant_id)
       invoice = create(:invoice, customer: customer, merchant: merchant )
-      invoice_item = create(:invoice_item, invoice: invoice, item: item, quantity: 40)
+      create(:invoice_item, invoice: invoice, item: item, quantity: 40)
 
-      invoice_2 = create(:invoice, customer: customer, merchant: merchant )
+      create(:invoice, customer: customer, merchant: merchant )
       invoice_item_2 = create(:invoice_item, invoice: invoice, item: item, created_at: 3.days.ago, quantity: 300)
 
       day = item.best_day
