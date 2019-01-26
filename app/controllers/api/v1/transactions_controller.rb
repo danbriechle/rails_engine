f86ad6226b82b070cc02527dd0af444a894fa927
Daniel Briechle <danbriechle@hotmail.com>
2019-01-25 18:11:46 -0700

    fixes typo in transaction factories addds test for transaction show endpoint

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -2,5 +2,4 @@ class Api::V1::TransactionsController < ApplicationController
   def index
     render json: TransactionSerializer.new(Transaction.all)
   end
-
 end
--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -7,4 +7,22 @@ describe "transactions API" do
     expect(response).to be_successful
     transactions = JSON.parse(response.body)
   end
+
+  it "can get one transaction by its id" do
+    item = create(:item)
+    customer = create(:customer)
+    merchant = Merchant.find(item.merchant_id)
+    invoice = create(:invoice, customer: customer, merchant: merchant )
+    id = create(:transaction, invoice: invoice).id
+
+
+    get "/api/v1/transactions/#{id}"
+
+    transaction_data= JSON.parse(response.body)
+
+    transaction = transaction_data["data"]
+
+    expect(response).to be_successful
+    expect(transaction["id"]).to eq(id.to_s)
+  end
 end
