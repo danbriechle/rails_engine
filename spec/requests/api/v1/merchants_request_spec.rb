2a08ab6342a7dfb52f4bfe562497ec4bd175026c
Daniel Briechle <danbriechle@hotmail.com>
2019-01-25 17:54:25 -0700

    adds merchant show test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,4 +7,18 @@ describe "Merchants API" do
     expect(response).to be_successful
     merchants = JSON.parse(response.body)
   end
+
+  it "can get one merchant by its id" do
+    id = create(:merchant).id
+
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant_data= JSON.parse(response.body)
+
+    merchant = merchant_data["data"]
+
+    expect(response).to be_successful
+    expect(merchant["id"]).to eq(id.to_s)
+  end
 end
