47a2427e66f96e0c305c5f90d7b69c13eda561eb
Daniel Briechle <danbriechle@hotmail.com>
2019-01-25 17:47:45 -0700

    adds test for getting one invoice by its id

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -1,10 +1,29 @@
 describe "Invoices API" do
-  it "sends a list of items" do
+  it "sends a list of invoices" do
     create_list(:item, 3)
 
     get '/api/v1/invoices'
 
     expect(response).to be_successful
     invoices = JSON.parse(response.body)
+  end
+
+  it "can get one invoice by its id" do
+    item = create(:item)
+    customer = create(:customer)
+    merchant = Merchant.find(item.merchant_id)
+    invoice = create(:invoice, customer: customer, merchant: merchant )
+
+    id = invoice.id
+
+
+    get "/api/v1/invoices/#{id}"
+
+    invoice_data= JSON.parse(response.body)
+
+    invoice = invoice_data["data"]
+
+    expect(response).to be_successful
+    expect(invoice["id"]).to eq(id.to_s)
   end
 end
