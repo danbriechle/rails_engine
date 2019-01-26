fc44dfc25b224857af3fdc4152b0dfff16288926
Daniel Briechle <danbriechle@hotmail.com>
2019-01-25 17:45:44 -0700

    adds invoice index route and controller with method

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
         resources :items, only: [:index, :show]
         resources :customers, only: [:index, :show]
         resources :invoice_items, only: [:index, :show]
+        resources :invoices, only: [:index]
       end
     end
   end
