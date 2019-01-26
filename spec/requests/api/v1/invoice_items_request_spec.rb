817901944cd6efa8c33fb1453f75c21eebcfb4ba
Daniel Briechle <danbriechle@hotmail.com>
2019-01-25 17:36:19 -0700

    adds test for finding an invoice by id

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -7,4 +7,18 @@ describe "Invoice_items API" do
     expect(response).to be_successful
     invoice_items = JSON.parse(response.body)
   end
+
+  it "can get one invoice by its id" do
+    id = create(:customer).id
+
+
+    get "/api/v1/invoices/#{id}"
+
+    invoice_data= JSON.parse(response.body)
+
+    invoice = invoice_data["data"]
+
+    expect(response).to be_successful
+    expect(invoice["id"]).to eq(id.to_s)
+  end
 end
