d7b0d1a439bd69ad9c90e6cba423657631d81ece
Daniel Briechle <danbriechle@hotmail.com>
2019-01-25 17:49:13 -0700

    adds show method and route to invoices controller

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -2,4 +2,7 @@ class Api::V1::InvoicesController < ApplicationController
   def index
     render json: InvoiceSerializer.new(Invoice.all)
   end
+  def show
+    render json: InvoiceSerializer.new(Invoice.find(params[:id]))
+  end
 end
