4249b72b9d5e89084a75f85dbdc74b69385f9dd3
Daniel Briechle <danbriechle@hotmail.com>
2019-01-25 19:01:39 -0700

    adds customer find by test

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -21,4 +21,17 @@ describe "Customers API" do
     expect(response).to be_successful
     expect(customer["id"]).to eq(id.to_s)
   end
+
+  it "can find a customer by its id " do
+    customer = create(:item)
+
+    get "/api/v1/customers/find?id=#{customer.id}"
+
+    customer_data= JSON.parse(response.body)
+
+    customer = customer_data["data"]
+
+    expect(response).to be_successful
+    expect(customer["id"]).to eq(id.to_s)
+  end
 end
