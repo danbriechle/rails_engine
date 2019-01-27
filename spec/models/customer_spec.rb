a2328ed68666fbe0fe0291d460a66853ef18386d
Daniel Briechle <danbriechle@hotmail.com>
2019-01-27 15:48:18 -0700

    adds favorite mechant model method

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -15,14 +15,15 @@ RSpec.describe Customer, type: :model do
       customer = create(:customer)
       merchant_1 = create(:merchant)
       merchant_2 = create(:merchant)
-      invoice_1 = create(:invoice, merchant_id: merchant_1.id)
-      invoice_2 = create(:invoice, merchant_id: merchant_1.id)
-      invoice_3 = create(:invoice, merchant_id: merchant_1.id)
+      invoice_1 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer.id)
+      invoice_2 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer.id)
+      invoice_3 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer.id)
 
-      invoice_4 = create(:invoice, merchant_id: merchant_2.id)
-      invoice_5 = create(:invoice, merchant_id: merchant_2.id)
+      invoice_4 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer.id)
+      invoice_5 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer.id)
 
-      expect(customer.favorite_merchant).to eq(merchant_1)
+      expect(customer.favorite_merchant).to eq([merchant_1])
+      expect(customer.favorite_merchant).to_not eq([merchant_2])
     end
   end
 end
