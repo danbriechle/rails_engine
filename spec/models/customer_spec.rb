65de7d77c6ecedb70fdb7c2e1eeb64e861c59f33
Daniel Briechle <danbriechle@hotmail.com>
2019-01-27 14:09:41 -0700

    adds test for favorite merchant model method

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Customer, type: :model do
   describe 'relationships' do
     it { should have_many :invoices}
   end
+
+  describe 'instance methods' do
+    it 'favorite_merchant' do
+      customer = create(:customer)
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      invoice_1 = create(:invoice, merchant_id: merchant_1.id)
+      invoice_2 = create(:invoice, merchant_id: merchant_1.id)
+      invoice_3 = create(:invoice, merchant_id: merchant_1.id)
+
+      invoice_4 = create(:invoice, merchant_id: merchant_2.id)
+      invoice_5 = create(:invoice, merchant_id: merchant_2.id)
+
+      expect(customer.favorite_merchant).to eq(merchant_1)
+    end
+  end
 end
